79717966be86485d36491922dc67340d8ab55328
Tereit <morten_mulvik@hotmail.com>
2018-02-07 15:07:54 +0100

    add: new actions and reducer to add new todos and toggle completed state

--- src/reducers/rootReducers.js
+++ src/reducers/rootReducers.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 
 // import actions here
+import todos from './todos';
 
 const rootReducer = combineReducers({
     // add the imported actions here
-    
+    todos
 });
 
 export default rootReducer;
